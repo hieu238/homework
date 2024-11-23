27dd5ad897131f23e161c8e520fbb74c50a13bf8
hieu238 <122993595+hieu238@users.noreply.github.com>
2024-11-23 16:11:11 +0700

    debug-code

---
+++ debug_code/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Program
+{
+    public static void Main(string[] args)
+    {
+        for(int i = 0; i < 10 ; i++)
+        {
+            System.Console.WriteLine("Index = " + i);
+        }
+    }
+}
