ef2dc5935552a2375f0c4caf25606c54ee5c87dd
hieu238 <122993595+hieu238@users.noreply.github.com>
2024-11-23 15:34:59 +0700

    checkyear

---
+++ checkyear/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Nhập năm cần kiểm tra: ");
+
+        int a = int.Parse(Console.ReadLine());
+
+        if(a % 400 == 0)
+        {
+            Console.WriteLine($"{a} là năm nhuận");
+        }
+        else if( a % 4 == 0 && a % 100 !=0)
+        {
+            Console.WriteLine($"{a} là năm nhuận");
+        }
+        else
+        {
+            Console.WriteLine($"{a} không phải là năm nhuận");
+        }
+    }
+}
